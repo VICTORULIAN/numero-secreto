984775ee9a977e8cc1581cd6b43828ef23563e1a
Victor ULian <victtorgabriel11.00@gmail.com>
2026-06-07 16:40:19 -0300

    teste

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ function verificarChute() {
     }
 }
 
+
+
+
+
 function gerarNumeroAleatorio() {
     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
